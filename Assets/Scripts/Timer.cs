8488ae134d12966f42b74b1720c36d4bf9ce6518
jiany1 <yao71@msn.com>
2016-10-15 13:43:04 -0700

    update timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -5,20 +5,53 @@ public class Timer : MonoBehaviour{
 
     public float timeLeft;
     public Text text;
+    public Text gameOver;
+    public int timeShow;
+    public int regularFont;
+    public int countDownFont;
 
     void Update()
     {
-        if (timeLeft > 0)
+        if (timeLeft >= 0)
         {
             timeLeft -= Time.deltaTime;
-            if (timeLeft <= 0)
+            timeShow = (int)timeLeft;
+            if (timeShow >= 30)
             {
-                text.text = "0:00";
+                text.fontSize = regularFont;
+                text.color = new Color(255, 255, 255);
+                if (timeShow >= 60)
+                {
+                    text.text = string.Format("{0:0}:{1:00}", Mathf.Floor(timeShow / 60), timeShow % 60);
+                }
+                else
+                {
+                    text.text = timeShow.ToString();
+                }
+            }
+            else if (timeShow >= 10)
+            {
+               
+                text.fontSize = countDownFont;
+                text.color = new Color(255, 255, 0);
+                text.text = timeShow.ToString();
+            }
+            else if (timeShow > 0)
+            {
+                text.fontSize = countDownFont;
+                text.color = new Color(255, 0, 0);
+                text.text = "0" + timeShow.ToString();
             }
             else
             {
-                text.text = string.Format("{0:0}:{1:00}", Mathf.Floor(timeLeft / 60), timeLeft % 60);
+                text.fontSize = countDownFont;
+                text.color = new Color(255, 0, 0);
+                text.text = "00";
+                gameOver.color = text.color;
+                gameOver.fontSize = 50;
+                gameOver.text = "GAME OVER";
             }
+            
         }
     }
 }
