2448260443fe5e7527c2e1287cac00accb1beb46
Remenes <yej9719@yahoo.com>
2016-10-14 22:16:08 -0700

    Added Camera Script
    Created basic Camera centered in the 2 game objects

---
+++ Assets/Scripts/Camera_Behavior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_Behavior : MonoBehaviour {
+
+    private Camera mainCamera;
+
+    public GameObject object1, object2;
+
+	// Use this for initialization
+	void Start () {
+        mainCamera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        //Puts camera between the 2 players
+        print(object1.transform.position + " | " + object2.transform.position + " | " + (object1.transform.position - object2.transform.position) / 2);
+        transform.position = (object1.transform.position + object2.transform.position) / 2 + new Vector3(0, 0, -10);
+	}
+
+
+}
