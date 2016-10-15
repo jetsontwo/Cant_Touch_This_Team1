6555d1cab88763daf9621aa068bd11c0d0cd3f40
jetsontwo <jetsontwo@gmail.com>
2016-10-14 21:54:23 -0700

    Updated Player Movement
    Updated player movement to directly change force rather than addForce.

--- Assets/Scripts/Player_Movements.cs
+++ Assets/Scripts/Player_Movements.cs
@@ -20,11 +20,11 @@ public class Player_Movements : MonoBehaviour {
 
         if (Input.GetKey(left_move))
         {
-            horiz_move += 1;
+            horiz_move -= 1;
         }
         if (Input.GetKey(right_move))
         {
-            horiz_move -= 1;
+            horiz_move += 1;
         }
 
         if (Input.GetKey(up_move))
@@ -36,9 +36,13 @@ public class Player_Movements : MonoBehaviour {
             vert_move -= 1;
         }
 
-        if (horiz_move != 0 && rb.velocity.magnitude <= max_vel)
+        if ((horiz_move != 0 || vert_move != 0) && rb.velocity.magnitude <= max_vel)
         {
-            rb.AddForce(new Vector2(horiz_move * speed, vert_move * speed));
+            rb.velocity += new Vector2(horiz_move * speed, vert_move * speed);
+        }
+        else
+        {
+            rb.velocity -= new Vector2(rb.velocity.x/5, rb.velocity.y/5);
         }
 	}
 }
