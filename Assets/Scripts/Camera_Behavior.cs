febdd22672741b8cc241c5d321bb16411990be28
Remenes <yej9719@yahoo.com>
2016-10-14 22:50:27 -0700

    Updated CameraBehavior
    Updated camera behavior to make it zoom when players move out of
    original boundaries

--- Assets/Scripts/Camera_Behavior.cs
+++ Assets/Scripts/Camera_Behavior.cs
@@ -4,19 +4,25 @@ using System.Collections;
 public class Camera_Behavior : MonoBehaviour {
 
     private Camera mainCamera;
+    private float cameraZoomRatio;
+    private float minCameraSize;
 
     public GameObject object1, object2;
 
 	// Use this for initialization
 	void Start () {
         mainCamera = GetComponent<Camera>();
+        cameraZoomRatio = 8;
+        minCameraSize = 5;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
         //Puts camera between the 2 players
-        print(object1.transform.position + " | " + object2.transform.position + " | " + (object1.transform.position - object2.transform.position) / 2);
         transform.position = (object1.transform.position + object2.transform.position) / 2 + new Vector3(0, 0, -10);
+        mainCamera.orthographicSize = Mathf.Clamp(Vector2.Distance(object1.transform.position, object2.transform.position) / cameraZoomRatio * 5 * mainCamera.aspect, minCameraSize, 50);
+
+        
 	}
 
 
