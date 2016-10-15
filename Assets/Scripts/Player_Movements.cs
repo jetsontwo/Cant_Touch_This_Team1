e589978b3e220d92fc043488f24f0404fc08db41
jetsontwo <jetsontwo@gmail.com>
2016-10-14 21:39:32 -0700

    Created Basic Movement Script
    Prototype movement

--- Assets/Scripts/Player_Movements.cs
+++ Assets/Scripts/Player_Movements.cs
@@ -3,13 +3,42 @@ using System.Collections;
 
 public class Player_Movements : MonoBehaviour {
 
+    public float speed, max_vel;
+    private Rigidbody2D rb;
+    public string left_move, right_move, up_move, down_move;
+    
+
 	// Use this for initialization
 	void Start () {
-	
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        int horiz_move = 0;
+        int vert_move = 0;
+
+        if (Input.GetKey(left_move))
+        {
+            horiz_move += 1;
+        }
+        if (Input.GetKey(right_move))
+        {
+            horiz_move -= 1;
+        }
+
+        if (Input.GetKey(up_move))
+        {
+            vert_move += 1;
+        }
+        if (Input.GetKey(down_move))
+        {
+            vert_move -= 1;
+        }
+
+        if (horiz_move != 0 && rb.velocity.magnitude <= max_vel)
+        {
+            rb.AddForce(new Vector2(horiz_move * speed, vert_move * speed));
+        }
 	}
 }
