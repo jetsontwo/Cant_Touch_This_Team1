c1a40100cb07bcafbba7ff4f750aa0295aff646b
Ted Won <tkwon09@gmail.com>
2016-10-14 22:28:04 -0700

    Added GameManagerScript, GamePlayerScript

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public float invincibilityMaxTimer;
+
+    private bool player1_is_it = false;
+    private float invincibilityTimer;
+    
+	void Start ()
+    {
+        invincibilityTimer = 0;
+        if (Random.value > 0.5)
+        {
+            player1_is_it = true;
+        }
+	}
+
+    public void NotifyTouched()
+    {
+        if (invincibilityTimer <= 0)
+        {
+            player1_is_it = !player1_is_it;
+            invincibilityTimer = invincibilityMaxTimer;
+        }
+    }
+	
+	void Update ()
+    {
+        if (invincibilityTimer > 0)
+        {
+            invincibilityTimer -= Time.deltaTime;
+        }
+	}
+}
