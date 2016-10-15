4d1c8eb2f60561e4da4fd9fbbaeb90dd31e3fb11
jetsontwo <jetsontwo@gmail.com>
2016-10-15 14:43:26 -0700

    Updated Player Movement with Falling

--- Assets/Scripts/Player_Movements.cs
+++ Assets/Scripts/Player_Movements.cs
@@ -7,8 +7,9 @@ public class Player_Movements : MonoBehaviour {
     private Rigidbody2D rb;
     public string left_move, right_move, up_move, down_move;
     public Animator am;
-    public bool water_movement;
+    public bool water_movement, falling;
     private float hop_timer_buffer;
+    private bool stop_falling;
 
 	// Use this for initialization
 	void Start () {
@@ -39,31 +40,42 @@ public class Player_Movements : MonoBehaviour {
             vert_move -= 1;
         }
 
-        if (water_movement)
+        if (!falling)
         {
-            if ((horiz_move != 0 || vert_move != 0) && rb.velocity.magnitude <= max_vel)
+            if (stop_falling)
+                rb.velocity = Vector2.zero;
+            if (water_movement)
             {
-                rb.velocity += new Vector2(horiz_move * acceleration, vert_move * acceleration);
+                if ((horiz_move != 0 || vert_move != 0) && rb.velocity.magnitude <= max_vel)
+                {
+                    rb.velocity += new Vector2(horiz_move * acceleration, vert_move * acceleration);
+                }
+                else
+                {
+                    rb.velocity -= new Vector2(rb.velocity.x / 10, rb.velocity.y / 10);
+                }
             }
             else
             {
-                rb.velocity -= new Vector2(rb.velocity.x / 10, rb.velocity.y / 10);
+                if ((horiz_move != 0 || vert_move != 0) && rb.velocity.magnitude <= max_vel && hop_timer <= 0)
+                {
+                    print("hi");
+                    rb.velocity += new Vector2(horiz_move * acceleration * hop_speed, vert_move * acceleration * hop_speed);
+                    hop_timer = hop_timer_buffer;
+                }
+                else
+                {
+                    rb.velocity -= new Vector2(rb.velocity.x / 10, rb.velocity.y / 10);
+                }
+                hop_timer -= hop_decrease;
             }
         }
         else
         {
-            if ((horiz_move != 0 || vert_move != 0) && rb.velocity.magnitude <= max_vel && hop_timer <= 0)
-            {
-                print("hi");
-                rb.velocity += new Vector2(horiz_move * acceleration * hop_speed, vert_move * acceleration * hop_speed);
-                hop_timer = hop_timer_buffer;
-            }
-            else
-            {
-                rb.velocity -= new Vector2(rb.velocity.x / 10, rb.velocity.y / 10);
-            }
-            hop_timer -= hop_decrease;
+            rb.velocity -= new Vector2(rb.velocity.x / 20, 1);
+            stop_falling = true;
         }
+        
 
 
         if (horiz_move != 0 || vert_move != 0)
