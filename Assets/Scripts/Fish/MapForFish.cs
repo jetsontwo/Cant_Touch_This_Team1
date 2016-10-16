acb2399f031e15219a965fef41d789621da89314
Ted Won <tkwon09@gmail.com>
2016-10-15 23:44:20 -0700

    Finished water flow, sense in water/out of water

--- Assets/Scripts/Fish/MapForFish.cs
+++ Assets/Scripts/Fish/MapForFish.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public enum State
 {
@@ -12,7 +12,15 @@ public class MapForFish : MonoBehaviour {
     
     public Sprite[] boardSprites;
     public Sprite wallSprite;
+    public Sprite waterSprite;
+    public Material spriteMat;
     public int sortingSubdivisions;
+    public float waterVolume;
+    public Vector2[] initialWaterArea;
+
+    private int waterArea;
+    private float[,] waterHeights = new float[8, 8];
+    private GameObject[,] waterTiles = new GameObject[8, 8];
 
     private int[,] heights = new int[,]{{2, 2, 2, 2, 2, 2, 2, 3},
                                         {2, 1, 2, 2, 2, 2, 2, 2},
@@ -20,8 +28,8 @@ public class MapForFish : MonoBehaviour {
                                         {0, 1, 2, 2, 2, 2, 2, 2},
                                         {0, 1, 2, 2, 2, 2, 2, 2},
                                         {0, 1, 2, 2, 2, 2, 2, 2},
-                                        {0, 1, 2, 0, 0, 0, 0, 0},
-                                        {0, 0, 0, 0, 0, 0, 0, 0}};
+                                        {1, 1, 2, 0, 0, 0, 0, 0},
+                                        {0, 0, 2, 0, 0, 0, 0, 0}};
 
     private int[,] tiles = new int[,]{{0, 0, 0, 0, 0, 0, 0, 1},
                                       {0, 1, 0, 0, 0, 0, 0, 0},
@@ -42,31 +50,185 @@ public class MapForFish : MonoBehaviour {
                 GameObject tileInstance = new GameObject();
                 tileInstance.AddComponent<SpriteRenderer>().sprite = boardSprites[tiles[i, j]];
                 tileInstance.GetComponent<SpriteRenderer>().sortingOrder = (-(int)j + heights[i, j]) * sortingSubdivisions;
+                tileInstance.GetComponent<SpriteRenderer>().material = spriteMat;
                 tileInstance.transform.position = position;
                 tileInstance.transform.parent = this.transform;
+                tileInstance.name = "tile: " + i + ", " + j;
                 for (int k = 1; k <= heights[i, j]; ++k)
                 {
                     Vector2 wallPosition = new Vector2(position.x, position.y - k);
                     GameObject wallInstance = new GameObject();
                     wallInstance.AddComponent<SpriteRenderer>().sprite = wallSprite;
                     wallInstance.GetComponent<SpriteRenderer>().sortingOrder = (-(int)j + heights[i, j]) * sortingSubdivisions;
+                    wallInstance.GetComponent<SpriteRenderer>().material = spriteMat;
                     wallInstance.transform.position = wallPosition;
                     wallInstance.transform.parent = this.transform;
+                    wallInstance.name = "wall: " + i + ", " + (j + k);
                 }
+            }
+        }
+        waterArea = initialWaterArea.Length;
+        for (int i = 0; i < waterTiles.GetLength(0); ++i)
+        {
+            for (int j = 0; j < waterTiles.GetLength(1); ++j)
+            {
+                waterHeights[i, j] = 0;
+                GameObject waterInstance = new GameObject();
+                waterInstance.AddComponent<SpriteRenderer>().sprite = waterSprite;
+                waterInstance.GetComponent<SpriteRenderer>().material = spriteMat;
+                waterTiles[i, j] = waterInstance;
+                waterInstance.name = "water: " + i + ", " + j;
+            }
+        }
+        for (int i = 0; i < initialWaterArea.Length; ++i)
+        {
+            waterHeights[(int)initialWaterArea[i].x, (int)initialWaterArea[i].y] = waterVolume / initialWaterArea.Length;
+        }
+        UpdateWaterSprites();
+    }
+
+    void Update()
+    {
+        HashSet<Vector2> newWaterArea = new HashSet<Vector2>();
+
+        // Spread water out
+        for (int i = 0; i < waterHeights.GetLength(0); ++i)
+        {
+            for (int j = 0; j < waterHeights.GetLength(1); ++j)
+            {
+                // check right
+                if (waterHeights[i, j] > 0 && i < waterHeights.GetLength(0) - 1 && heights[i + 1, j] + waterHeights[i + 1, j] < heights[i, j] + waterHeights[i, j])
+                {
+                    if (waterHeights[i + 1, j] == 0)
+                    {
+                        ++waterArea;
+                    }
+                    float diff = (waterHeights[i, j] + heights[i, j] + waterHeights[i + 1, j] + heights[i + 1, j]) / 2f;
+                    waterHeights[i + 1, j] = diff - heights[i + 1, j];
+                    waterHeights[i, j] = diff - heights[i, j];
+                    if (waterHeights[i, j] <= 0)
+                    {
+                        waterHeights[i, j] = 0;
+                        --waterArea;
+                    }
+                    if (waterHeights[i + 1, j] <= 0)
+                    {
+                        waterHeights[i + 1, j] = 0;
+                        --waterArea;
+                    }
+                }
+                // check left
+                if (waterHeights[i, j] > 0 && i > 0 && heights[i - 1, j] + waterHeights[i - 1, j] < heights[i, j] + waterHeights[i, j])
+                {
+                    if (waterHeights[i - 1, j] == 0)
+                    {
+                        ++waterArea;
+                    }
+                    float diff = (waterHeights[i, j] + heights[i, j] + waterHeights[i - 1, j] + heights[i - 1, j]) / 2f;
+                    waterHeights[i - 1, j] = diff - heights[i - 1, j];
+                    waterHeights[i, j] = diff - heights[i, j];
+                    if (waterHeights[i, j] <= 0)
+                    {
+                        waterHeights[i, j] = 0;
+                        --waterArea;
+                    }
+                    if (waterHeights[i - 1, j] <= 0)
+                    {
+                        waterHeights[i - 1, j] = 0;
+                        --waterArea;
+                    }
+                }
+                // check up
+                if (waterHeights[i, j] > 0 && j < waterHeights.GetLength(1) - 1 && heights[i, j + 1] + waterHeights[i, j + 1] < heights[i, j] + waterHeights[i, j])
+                {
+                    if (waterHeights[i, j + 1] == 0)
+                    {
+                        ++waterArea;
+                    }
+                    float diff = (waterHeights[i, j] + heights[i, j] + waterHeights[i, j + 1] + heights[i, j + 1]) / 2f;
+                    waterHeights[i, j + 1] = diff - heights[i, j + 1];
+                    waterHeights[i, j] = diff - heights[i, j];
+                    if (waterHeights[i, j] <= 0)
+                    {
+                        waterHeights[i, j] = 0;
+                        --waterArea;
+                    }
+                    if (waterHeights[i, j + 1] <= 0)
+                    {
+                        waterHeights[i, j + 1] = 0;
+                        --waterArea;
+                    }
+                }
+                // check down
+                if (waterHeights[i, j] > 0 && j > 0 && heights[i, j - 1] + waterHeights[i, j - 1] < heights[i, j] + waterHeights[i, j])
+                {
+                    if (waterHeights[i, j - 1] == 0)
+                    {
+                        ++waterArea;
+                    }
+                    float diff = (waterHeights[i, j] + heights[i, j] + waterHeights[i, j - 1] + heights[i, j - 1]) / 2f;
+                    waterHeights[i, j - 1] = diff - heights[i, j - 1];
+                    waterHeights[i, j] = diff - heights[i, j];
+                    if (waterHeights[i, j] <= 0)
+                    {
+                        waterHeights[i, j] = 0;
+                        --waterArea;
+                    }
+                    if (waterHeights[i, j - 1] <= 0)
+                    {
+                        waterHeights[i, j - 1] = 0;
+                        --waterArea;
+                    }
+                }
+            }
+        }
+        // Update water sprites
+        UpdateWaterSprites();
+    }
+
+    void UpdateWaterSprites()
+    {
+        for (int i = 0; i < waterTiles.GetLength(0); ++i)
+        {
+            for (int j = 0; j < waterTiles.GetLength(1); ++j)
+            {
+                if (waterTiles[i, j].GetComponent<SpriteRenderer>().enabled)
+                {
+                    if (waterHeights[i, j] <= 0)
+                    {
+                        waterTiles[i, j].GetComponent<SpriteRenderer>().enabled = false;
+                    }
+                    waterTiles[i, j].GetComponent<SpriteRenderer>().sortingOrder = (-(int)j + heights[i, j] + Mathf.CeilToInt(waterHeights[i, j])) * sortingSubdivisions;
+                    waterTiles[i, j].transform.position = new Vector2(i, j + heights[i, j] + waterHeights[i, j]);
+                }
+                else
+                {
+                    if (waterHeights[i, j] > 0)
+                    {
+                        waterTiles[i, j].GetComponent<SpriteRenderer>().enabled = true;
+                    }
+                }
+
             }
         }
     }
 
-    public void GetTile(float screenx, float screeny, int height, out int x, out int y, out int newheight)
+    public void GetTile(float screenx, float screeny, int height, float waterHeight, out int x, out int y, out int newheight, out float newWaterHeight)
     {
         x = Mathf.RoundToInt(screenx);
-        y = Mathf.RoundToInt(screeny - height);
+        y = Mathf.RoundToInt(screeny - height - waterHeight);
         newheight = heights[x, y];
+        newWaterHeight = waterHeights[x, y];
     }
 
     public int GetHeightAt(int x, int y)
     {
         return heights[x, y];
+    }
+
+    public float GetWaterHeightAt(int x, int y)
+    {
+        return waterHeights[x, y];
     }
 
     public int GetWidth()
--- Assets/Scripts/Fish/MapPositionSensor.cs
+++ Assets/Scripts/Fish/MapPositionSensor.cs
@@ -12,52 +12,55 @@ public class MapPositionSensor : MonoBehaviour
     public int x;
     public int y;
     public int height;
+    public float waterheight;
+    public float minWaterHeight;
 
     // Use this for initialization
     void Start()
     {
         map = FindObjectOfType<MapForFish>();
-        map.GetTile(this.transform.position.x, this.transform.position.y, map.GetHeightAt(x, y), out x, out y, out height);
+        map.GetTile(this.transform.position.x, this.transform.position.y, map.GetHeightAt(x, y), map.GetWaterHeightAt(x, y), out x, out y, out height, out waterheight);
         movements = GetComponent<Player_Movements>();
         lastPos = this.transform.position;
     }
 
     // Update is called once per frame
-    // @TODO: Animations should play regardless of whether player can go to the next space or not.
     void Update()
     {
         int newheight, newx, newy;
-        
+        float newwaterheight;
+
         // Prevent movement out of bounds
         if (this.transform.position.x < 0)
         {
             this.transform.position = new Vector3(0, this.transform.position.y, 0);
         }
-        if (this.transform.position.y < height)
+        if (this.transform.position.y < height + waterheight)
         {
-            this.transform.position = new Vector3(this.transform.position.x, height, 0);
+            this.transform.position = new Vector3(this.transform.position.x, height + waterheight, 0);
         }
         if (this.transform.position.x > map.GetWidth() - 1)
         {
             this.transform.position = new Vector3(map.GetWidth() - 1, this.transform.position.y, 0);
         }
-        if (this.transform.position.y > map.GetHeight() + height - 1)
+        if (this.transform.position.y > map.GetHeight() + height + waterheight - 1)
         {
-            this.transform.position = new Vector3(this.transform.position.x, map.GetHeight() + height - 1, 0);
+            this.transform.position = new Vector3(this.transform.position.x, map.GetHeight() + height + waterheight - 1, 0);
         }
 
         // Get tile information from the map
         if (!movements.falling)
         {
-            map.GetTile(this.transform.position.x, this.transform.position.y, height, out newx, out newy, out newheight);
-            if (newheight < height)
+            map.GetTile(this.transform.position.x, this.transform.position.y, height, waterheight, out newx, out newy, out newheight, out newwaterheight);
+            if (newheight + Mathf.FloorToInt(newwaterheight) < height + Mathf.FloorToInt(waterheight))
             {
                 movements.falling = true;
                 x = newx;
                 y = newy;
                 height = newheight;
+                waterheight = newwaterheight;
             }
-            else if (!movements.falling && newheight > height)
+            else if (newheight + Mathf.FloorToInt(newwaterheight) > height + Mathf.FloorToInt(waterheight))
             {
                 if (newy != y)
                 {
@@ -74,24 +77,51 @@ public class MapPositionSensor : MonoBehaviour
             {
                 x = newx;
                 y = newy;
+                waterheight = newwaterheight;
+                height = newheight;
             }
-            shadow.transform.position = this.transform.position;
+
+            if (waterheight > minWaterHeight)
+            {
+                if (!movements.water_movement)
+                {
+                    movements.water_movement = true;
+                }
+            }
+            else
+            {
+                if (movements.water_movement)
+                {
+                    movements.water_movement = false;
+                }
+            }
+
+            if (movements.water_movement)
+            {
+                sprite.sortingOrder = (-y + map.GetHeightAt(x, y) + Mathf.FloorToInt(map.GetWaterHeightAt(x, y))) * map.sortingSubdivisions + 2;
+            }
+            else
+            {
+                sprite.sortingOrder = (-y + map.GetHeightAt(x, y)) * map.sortingSubdivisions + 2;
+            }
+            shadow.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - waterheight, 0);
+            float shadowScale = 0.2f + Mathf.Pow(0.5f, waterheight);
+            shadow.transform.localScale = new Vector3(shadowScale, shadowScale, 0);
         }
         else
         {
-            if (this.transform.position.y <= (y + height))
+            if (this.transform.position.y <= (y + height + waterheight))
             {
                 movements.falling = false;
             }
             shadow.transform.position = new Vector3(this.transform.position.x, y + height, 0);
-            float shadowScale = 1.5f - (this.transform.position.y - y - height);
+            float shadowScale = 1f - (this.transform.position.y - y - height);
             if (shadowScale < 0.5f)
             {
                 shadowScale = 0.5f;
             }
             shadow.transform.localScale = new Vector3(shadowScale, shadowScale, 0);
         }
-        sprite.sortingOrder = (-y + map.GetHeightAt(x, y))*map.sortingSubdivisions + 2;
         shadow.GetComponent<SpriteRenderer>().sortingOrder = sprite.sortingOrder - 1;
         lastPos = this.transform.position;
     }
