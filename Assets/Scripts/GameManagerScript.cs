7367fa3144fd3ee15950194a6141123aa293d05f
Remenes <yej9719@yahoo.com>
2016-10-15 20:09:35 -0700

    Updated GameManager Script to allow player collisions
    Game Manager now allows collisions between fishes and lets the targeted
    fish push the other away while making them spin.

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -12,8 +12,10 @@ public class GameManagerScript : MonoBehaviour
     public Text gameOver;
     public Button backToMain;
     public Button playAgain;
-    public Object player1;
-    public Object player2;
+    public GameObject player1;
+    public GameObject player2;
+    private Rigidbody2D player1rb, player2rb;
+    private float stunDuration;
 
     private bool player1_is_it = false;
     private float invincibilityTimer;
@@ -24,13 +26,16 @@ public class GameManagerScript : MonoBehaviour
     private Color white = new Color(255, 255, 255);
     private Color red = new Color(255, 0, 0);
 
-    
+    private IEnumerator spinCoroutine;
 
 	void Start ()
     {
         invincibilityTimer = 0;
         player1_score = 0;
         player2_score = 0;
+        player1rb = player1.GetComponent<Rigidbody2D>();
+        player2rb = player2.GetComponent<Rigidbody2D>();
+
         backToMain.gameObject.SetActive(false);
         playAgain.gameObject.SetActive(false);
         if (Random.value > 0.5)
@@ -38,21 +43,68 @@ public class GameManagerScript : MonoBehaviour
             player1_is_it = true;
         }
         gameTimer.timeLeft = gameMaxTimer;
+
+        stunDuration = 1.25f;
     }
     
     public void NotifyTouched()
     {
-        if (invincibilityTimer <= 0)
-        {
-            player1_is_it = !player1_is_it;
-            invincibilityTimer = invincibilityMaxTimer;
+        if (player1rb.IsTouching(player2.GetComponent<Collider2D>())) {
+            if (invincibilityTimer <= 0) {
+                stunPlayer();
+                player1_is_it = !player1_is_it;
+                invincibilityTimer = invincibilityMaxTimer;
+            }
         }
+
+    }
+
+    public void stunPlayer() {
+        //Stun the player who is not it
+        GameObject stunnedPlayer = player1_is_it ? player1 : player2;
+        GameObject otherPlayer = player1_is_it ? player2 : player1;
+
+        stunnedPlayer.GetComponent<Player_Movements>().hop_timer = 100;
+        Vector2 newVelocity = (stunnedPlayer.transform.position - otherPlayer.transform.position).normalized * 100;
+        player2rb.velocity = newVelocity;
+
+        spinCoroutine = spinPlayer(1300, stunnedPlayer);
+        StartCoroutine(spinCoroutine);
+
+        //if (player1_is_it) {
+        //    player2.GetComponent<Player_Movements>().hop_timer = 100;
+        //    Vector2 newVelocity = (player2rb.position - player1rb.position).normalized * 100;
+        //    print(newVelocity);
+        //    player2rb.velocity = newVelocity;
+        //}
+        //else { //Player 2 is it
+        //    player1.GetComponent<Player_Movements>().hop_timer = 100;
+        //    Vector2 newVelocity = (player1rb.position - player2rb.position).normalized * 100;
+        //    print(newVelocity);
+        //    player1rb.velocity = newVelocity;
+        //}
+    }
+
+    private IEnumerator spinPlayer(float anglesPerSecond, GameObject player) {
+        float delay = 0.05f;
+        float stunTimeLeft = stunDuration;
+        int rotateLeftOrRight = Random.Range(-1, 1) >= 0 ? 1 : -1;
+        Vector3 spinVector = new Vector3(0, 0, anglesPerSecond * delay * rotateLeftOrRight);
+
+        while (stunTimeLeft > 0) {
+            player.transform.Rotate(spinVector);
+            stunTimeLeft -= delay;
+            yield return new WaitForSeconds(delay);
+        }
+        player.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        
     }
 	
 	void Update ()
     {
         if ((int)gameTimer.timeLeft > 0)
         {
+            NotifyTouched();
             if (invincibilityTimer >= 0)
             {
                 invincibilityTimer -= Time.deltaTime;
